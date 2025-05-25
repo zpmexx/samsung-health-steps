f2a0380f918c581a5755f70617ed61c996211d2b
ZP <ziemowitpalka@gmail.com>
2025-05-25 15:53:46 +0200

    Readme filled with function's outputs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import glob
 import os
+import matplotlib.pyplot as plt
 from config import main_config
 
 # load data from CSV files 
@@ -57,7 +58,7 @@ def get_median_steps(dates_steps):
     # Calculate the median steps
     return int(dates_steps['count'].median().round(0))
 
-def get_top_steps(dates_steps, top_number):
+def get_top_n_days(dates_steps, top_number):
     # Get the top n days with the most steps
     return dates_steps.nlargest(top_number, 'count')
 
@@ -94,8 +95,39 @@ def get_avg_steps_per_weekday(dates_steps):
     dates_steps['weekday'] = dates_steps.index.day_name()
     weekday_avg = dates_steps.groupby('weekday')['count'].mean().reset_index()
     weekday_avg['count'] = weekday_avg['count'].round(0).astype(int)
+    weekday_avg = weekday_avg.sort_values('count', ascending=False)
+
     return weekday_avg
 
+def get_steps_per_month(dates_steps):
+    # Get the steps per month
+    dates_steps.index = pd.to_datetime(dates_steps.index)
+    dates_steps['month'] = dates_steps.index.to_period('M')
+    monthly_avg = dates_steps.groupby('month')['count'].mean().reset_index()
+    monthly_avg['month'] = monthly_avg['month'].dt.to_timestamp()
+
+    monthly_avg['month'] = monthly_avg['month'].dt.strftime('%Y-%m')
+    monthly_avg['count'] = monthly_avg['count'].round(0).astype(int)
+    
+    return monthly_avg
+
+def plot_avg_steps_per_month(months_avg):
+    # Make sure 'month' column is in datetime format
+    months_avg['month'] = pd.to_datetime(months_avg['month'])
+
+    # Plotting
+    plt.figure(figsize=(10, 5))
+    plt.plot(months_avg['month'], months_avg['count'], marker='o', linestyle='-', color='blue')
+    plt.title('Average Steps per Month')
+    plt.xlabel('Month')
+    plt.ylabel('Average Steps')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    # Save and show the plot
+    output_path = "avg_steps_per_month.png"
+    plt.savefig(output_path)
 
 # Load data from CSV files
 data = load_data()
@@ -112,7 +144,7 @@ target_days_count, total_days_count = count_target_days(dates_steps)
 
 avg_steps = get_average_steps(dates_steps)
 median_steps = get_median_steps(dates_steps)
-top_n_steps = get_top_steps(dates_steps, main_config['top_number'])
+top_n_days = get_top_n_days(dates_steps, main_config['top_number'])
 
 
 #print(top_n_steps)
@@ -124,10 +156,14 @@ avg_steps_per_month = get_avg_steps_per_month(dates_steps)
 #print(get_avg_steps_per_weekday(dates_steps))
 
 
-print(get_steps_list_to_compare(dates_steps))
+markdown = f"""[Configuration](#configuration) |
+[Target Days Summary](#target-days-summary) |
+[Overall Steps Summary](#overall-steps-summary) |
+[Top Days](#top-days) |
+[Average Steps per Weekday](#average-steps-per-weekday) |
+[Avarege Steps per Month](#average-steps-per-month)
 
-markdown = f"""[Configuration](#configuration)\n
-[Target Days Summary](#target-days-summary)\n"""
+"""
 
 
 
@@ -137,7 +173,7 @@ markdown += f"""\n ### Overall Steps Summary\n
 | Days               |   {total_days_count}    |
 | Average      |   {avg_steps}     |
 | Median       |   {median_steps}     |
-| Top           |  {top_n_steps['count'].max()}     |
+| Top           |  {top_n_days['count'].max()}     |
 | Daily target           |  {main_config['steps_target']}    |
 """
 
@@ -148,7 +184,24 @@ for steps, (days, percent) in get_steps_list_to_compare(dates_steps).items():
     markdown += f"| {steps}         | {days}           | {percent}% |\n"
 
 
+markdown += "\n### Top Days\n\n"
+markdown += "|   | Count | Date |\n"
+markdown += "|---|--------------|------|\n"
 
+for i, (date, row) in enumerate(top_n_days.iterrows(), start=1):
+    markdown += f"| {i} | {row['count']} | {date} |\n"
+
+
+markdown += "\n### Average Steps per Weekday\n\n"
+markdown += "| Weekday   | Average Steps |\n"
+markdown += "|-----------|----------------|\n"
+
+for _, row in get_avg_steps_per_weekday(dates_steps).iterrows():
+    markdown += f"| {row['weekday']} | {row['count']} |\n"
+
+
+markdown += "\n### Avg Steps per Month\n\n"
+markdown += "![Average Steps per Month](avg_steps_per_month.png)\n"
 
 configuration_markdown = f"""
 # Configuration
@@ -158,5 +211,10 @@ configuration_markdown = f"""
 """
 markdown += configuration_markdown
 
-with open('README1.md', 'w') as f:
+with open('README.md', 'w') as f:
     f.write(markdown)
+    
+months_avg = get_steps_per_month(dates_steps)
+
+plot_avg_steps_per_month(months_avg)
+
